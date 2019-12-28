e34bc2a42b4ec5584a9f959edd96ee0c3c1ad713
Goldenlion5648 <coboudinot@gmail.com>
2019-12-28 17:29:53 -0500

    working movement and numbering

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -18,6 +18,9 @@ public class CameraControl : MonoBehaviour
     private float yaw = 0.0f;
     private float pitch = 0.0f;
 
+    public float movementSpeed = 30.0f;
+
+
     void Update()
     {
         yaw += speedH * Input.GetAxis("Mouse X");
@@ -29,13 +32,73 @@ public class CameraControl : MonoBehaviour
         {
             transform.position += transform.rotation.eulerAngles;
         }
+        //GameObject.Find("PlayerCamera")
+        Vector3 pos = transform.position;
+        if (Input.GetButtonDown("Fire1"))
+        {
 
+        }
+
+        //if (Input.GetKeyDown(KeyCode.A))
+        //{
+        //    pos.x += 3;
+        //    Debug.Log("pressed a");
+
+        //}
+        float time = Time.deltaTime * 10; 
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            pos += Camera.main.transform.forward * movementSpeed * time;
+
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            pos -= Camera.main.transform.forward * movementSpeed * time;
+
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            pos += Vector3.Cross(Camera.main.transform.forward, Vector3.up) * movementSpeed * time;
+
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            pos -= Vector3.Cross(Camera.main.transform.forward, Vector3.up) * movementSpeed * time;
+
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            pos.y += movementSpeed * time / 2;
+
+        }
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            pos.y -= movementSpeed * time / 2;
+
+        }
+        transform.position = pos;
+
+        //if (Input.GetKeyDown(KeyCode.Space))
+        //{
+        //    Vec
+
+        //}
 
 
         if (Input.GetMouseButtonDown(1))
         {
             Cursor.visible = true;
             Cursor.lockState = CursorLockMode.None;
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            UnityEditor.EditorApplication.isPlaying = false;
+
         }
     }
 }
--- Assets/Scripts/SceneScript.cs
+++ Assets/Scripts/SceneScript.cs
@@ -6,33 +6,49 @@ public class SceneScript : MonoBehaviour
 {
     // Start is called before the first frame update
     public Transform prefab;
+
+ 
     void Start()
     {
+        Transform test = prefab.transform;
+        
         int cubeDim = 3;
-        Vector3 startPos = new Vector3(0, 1.5f, 0);
+        
+        Vector3 startPos = prefab.position;
 
-        float sideLength = 15;
+        float sideLength = 6;
 
-        float individualSideLength = sideLength / cubeDim + 2;
+        float cubeSpacing = sideLength / cubeDim + 2;
 
         float curXPos = 0;
         float curYPos = 1.5f;
         float curZPos = 0;
         for (int i = 0; i < (int)Mathf.Pow(cubeDim, 3); i++)
         {
+            GameObject text = new GameObject();
+            TextMesh t = text.AddComponent<TextMesh>();
+           
+
+            t.fontSize = 20;
+
             if (i % 3 == 0)
             {
-                curXPos += individualSideLength;
+                curXPos -= cubeSpacing;
                 curZPos = startPos.z;
 
             }
             if (i % 9 == 0)
             {
-                curYPos += individualSideLength;
+                curYPos += cubeSpacing;
                 curXPos = startPos.x;
             }
-            curZPos += individualSideLength;
-            Instantiate(prefab, new Vector3(curXPos, curYPos, curZPos), Quaternion.identity);
+            curZPos += cubeSpacing;
+            Transform x = Instantiate(prefab, new Vector3(curXPos, curYPos, curZPos), Quaternion.identity);
+            t.anchor = TextAnchor.MiddleCenter;
+            t.transform.position = new Vector3(x.transform.position.x , x.transform.position.y, x.transform.position.z - .5f);
+            t.text = i.ToString();
+
+            
 
             Debug.Log("made new Cube");
         }
