410652858ecf4c6ab7cc5e010cc6d90ecbdb0a4d
Goldenlion5648 <coboudinot@gmail.com>
2019-12-28 16:11:18 -0500

    first commit2

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        //Cursor.visible = false;
+    }
+
+
+    public float speedH = 2.0f;
+    public float speedV = 2.0f;
+
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
+
+    void Update()
+    {
+        yaw += speedH * Input.GetAxis("Mouse X");
+        pitch -= speedV * Input.GetAxis("Mouse Y");
+
+        transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+
+        if(Input.mouseScrollDelta.y > 1)
+        {
+            transform.position += transform.rotation.eulerAngles;
+        }
+
+
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+}
---
+++ Assets/Scripts/SceneScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform prefab;
+    void Start()
+    {
+        int cubeDim = 3;
+        Vector3 startPos = new Vector3(0, 1.5f, 0);
+
+        float sideLength = 15;
+
+        float individualSideLength = sideLength / cubeDim + 2;
+
+        float curXPos = 0;
+        float curYPos = 1.5f;
+        float curZPos = 0;
+        for (int i = 0; i < (int)Mathf.Pow(cubeDim, 3); i++)
+        {
+            if (i % 3 == 0)
+            {
+                curXPos += individualSideLength;
+                curZPos = startPos.z;
+
+            }
+            if (i % 9 == 0)
+            {
+                curYPos += individualSideLength;
+                curXPos = startPos.x;
+            }
+            curZPos += individualSideLength;
+            Instantiate(prefab, new Vector3(curXPos, curYPos, curZPos), Quaternion.identity);
+
+            Debug.Log("made new Cube");
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
