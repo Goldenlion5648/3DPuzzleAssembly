1f7f69dc4b228caee40b073ed0f82712a3fcb573
Goldenlion5648 <coboudinot@gmail.com>
2019-12-28 19:11:48 -0500

    change to nested for loops

--- Assets/Scripts/SceneScript.cs
+++ Assets/Scripts/SceneScript.cs
@@ -23,35 +23,65 @@ public class SceneScript : MonoBehaviour
         float curXPos = 0;
         float curYPos = 1.5f;
         float curZPos = 0;
-        for (int i = 0; i < (int)Mathf.Pow(cubeDim, 3); i++)
+        int total = 0;
+        for (int y = 0; y < cubeDim; y++)
         {
-            GameObject text = new GameObject();
-            TextMesh t = text.AddComponent<TextMesh>();
-           
-
-            t.fontSize = 20;
-
-            if (i % 3 == 0)
+            for (int x = 0; x < cubeDim; x++)
             {
+                for (int z = 0; z < cubeDim; z++)
+                {
+                    GameObject text = new GameObject();
+                    TextMesh t = text.AddComponent<TextMesh>();
+
+
+                    t.fontSize = 20;
+                    curZPos += cubeSpacing;
+                    Transform newCube = Instantiate(prefab, new Vector3(curXPos, curYPos, curZPos), Quaternion.identity);
+                    t.anchor = TextAnchor.MiddleCenter;
+                    t.transform.position = new Vector3(newCube.transform.position.x, newCube.transform.position.y, newCube.transform.position.z - .5f);
+                    t.text = total.ToString();
+
+
+                    total++;
+                    Debug.Log("made new Cube");
+                }
                 curXPos -= cubeSpacing;
                 curZPos = startPos.z;
-
             }
-            if (i % 9 == 0)
-            {
-                curYPos += cubeSpacing;
-                curXPos = startPos.x;
-            }
-            curZPos += cubeSpacing;
-            Transform x = Instantiate(prefab, new Vector3(curXPos, curYPos, curZPos), Quaternion.identity);
-            t.anchor = TextAnchor.MiddleCenter;
-            t.transform.position = new Vector3(x.transform.position.x , x.transform.position.y, x.transform.position.z - .5f);
-            t.text = i.ToString();
-
-            
-
-            Debug.Log("made new Cube");
+            curYPos += cubeSpacing;
+            curXPos = startPos.x;
         }
+
+
+        //for (int i = 0; i < (int)Mathf.Pow(cubeDim, 3); i++)
+        //{
+        //    GameObject text = new GameObject();
+        //    TextMesh t = text.AddComponent<TextMesh>();
+
+
+        //    t.fontSize = 20;
+
+        //    if (i % 3 == 0)
+        //    {
+        //        curXPos -= cubeSpacing;
+        //        curZPos = startPos.z;
+
+        //    }
+        //    if (i % 9 == 0)
+        //    {
+        //        curYPos += cubeSpacing;
+        //        curXPos = startPos.x;
+        //    }
+        //    curZPos += cubeSpacing;
+        //    Transform x = Instantiate(prefab, new Vector3(curXPos, curYPos, curZPos), Quaternion.identity);
+        //    t.anchor = TextAnchor.MiddleCenter;
+        //    t.transform.position = new Vector3(x.transform.position.x , x.transform.position.y, x.transform.position.z - .5f);
+        //    t.text = i.ToString();
+
+
+
+        //    Debug.Log("made new Cube");
+        //}
 
     }
 
